3db65fc6849d628038ae186d4c3c8139e278154a
Janay Harris <jha2972@wgu.edu>
2024-06-16 02:01:59 -0500

    net does not work,🐋 time

--- .netlify/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.KGkCqsnV.js","_app/immutable/chunks/scheduler.DvUPq92Z.js","_app/immutable/chunks/index.Dtlhvem1.js"];
-export const stylesheets = ["_app/immutable/assets/0.ChspH-69.css"];
-export const fonts = [];
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 
 export default {
 	kit: {
