ee07ac011b652d00b07cbc5477e205b9fff4351a
Janay Harris <jha2972@wgu.edu>
2024-06-16 14:12:08 -0500

    Update svelte.config.js to remove deprecated options

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
-		target: '#svelte'
-	},
-	preprocess: preprocess()
+		adapter: adapter()
+	}
 };
 
 export default config;
